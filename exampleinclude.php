4d91b38f3ec4f234b2106d9b2e74b1c6a7c6b420
Daniel Vygolov <rattack@gmail.com>
2021-10-26 00:48:24 +0400

    added examples

--- exampleinclude.php
+++ exampleinclude.php
@@ -2,5 +2,5 @@
 $lang='hi';
 $template='m1';
 $pixel_event='Purchase';
-include 'index.php';
+include '../thankyou/index.php';
 ?>
---
+++ exampleredirect.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: index.php?px=12345&lang=ru&template=shakes&pixelevent=Purchase');
+?>
